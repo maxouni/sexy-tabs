cf3317a6204d7cdd02a2cb5b07960301a8300174
Max Mishin <max@mishin.io>
2019-05-29 18:07:18 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/style.scss',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './examples/index.html'
+    })],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader"
+            }]
+
+        }]
+    }
+};
